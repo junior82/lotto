2ab1abec2e1da5b9e60244cbd1506dd1693bb019
jykim@diquest.com <jykim@diquest.com>
2018-07-14 06:05:25 +0000

    hotfix2

--- src/project/Lotto.java
+++ src/project/Lotto.java
@@ -1,16 +1,30 @@
 package project;
 
 import java.util.Random;
+import java.util.Set;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 public class Lotto {
     private Random random = new Random(System.nanoTime());
     private int[] generate(){
         int[] result = new int[6];
+        int index = 0;
+        Set<Integer> generated = new HashSet<>();
         
-        for(int i =0 ; i < 6 ; i++){
-            result[i] = random.nextInt(45) + 1;
+        while(generated.size() < 6){
+            int num = random.nextInt(45) + 1;
+            
+            if(!contains(generated, num)){
+                result[index++] = num;
+                generated.add(num);
+            }
         }
         return result;
+    }
+    
+    boolean contains(Set<Integer> generated,int num){
+        return generated.contains(num);
     }
     
     public static void main(String[] args) {
