87cafd8c05b5ebe52a59fa31e1fec734ae1a9346
jykim@diquest.com <jykim@diquest.com>
2018-07-14 05:14:42 +0000

    Lotto

---
+++ src/project/Lotto.java
@@ -0,0 +1,20 @@
+package project;
+
+import java.util.Random;
+import java.util.Arrays;
+public class Lotto {
+    private Random random = new Random(System.nanoTime());
+    private int[] generate(){
+        int[] result = new int[6];
+        
+        for(int i =0 ; i < 6 ; i++){
+            result[i] = random.nextInt(45);
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        int[] result = new Lotto().generate();
+        System.out.println(Arrays.toString(result));
+    }
+}
