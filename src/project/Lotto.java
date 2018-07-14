97cd618bcfdc48d5075bc218fc1e0b4dcef7d8b6
jykim@diquest.com <jykim@diquest.com>
2018-07-14 05:19:15 +0000

    issus complte

--- src/project/Lotto.java
+++ src/project/Lotto.java
@@ -8,7 +8,7 @@ public class Lotto {
         int[] result = new int[6];
         
         for(int i =0 ; i < 6 ; i++){
-            result[i] = random.nextInt(45);
+            result[i] = random.nextInt(45) + 1;
         }
         return result;
     }
